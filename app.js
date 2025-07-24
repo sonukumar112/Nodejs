a4c286d08133eb2d782a3a5d985e9308c9854c87
Sonu Kumar Sah <sonusah22388@gmail.com>
2025-07-24 23:05:21 +0530

    added code

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Learning NodeJs</title></head>');
+  res.write('<body><h1>Hello from Node.js!</h1></body>');
+  //res.write('</html>');
 });
 
 const PORT = 3000;
